29f64ca1375f36ac9c48a322437a3954b0b543ac
karol lopes <125413886+karolllopes@users.noreply.github.com>
2024-03-05 07:57:23 -0300

    Create Line1.kt

---
+++ src/jsMain/kotlin/Line1.kt
@@ -0,0 +1,18 @@
+package kotlin
+
+incline fun transacao(funcao: () -> Unit) {
+	println("abrindo transação...")
+	try {
+	     funcao()	
+	}finally {
+	     println("fechando trnsação")
+	}
+}
+
+fun main(args: Array<String>) {
+	transacao {
+	    println("Executando SQL 1...")
+	    println("Executando SQL 2...")
+	    println("Executando SQL 3...")
+	}
+}
